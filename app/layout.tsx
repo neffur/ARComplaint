0abc9456183b76539697bcbe7aeb8639b863388c
neffur <neffuriouz@gmail.com>
2026-05-17 22:42:46 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: "#0f0f0f",
+  themeColor: "#080808",
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
